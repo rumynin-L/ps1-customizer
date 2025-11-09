4fafaa130e5dcdd7a4ee0faaa53866d9d206a5ac
rumynin-L <pablormlu@gmail.com>
2025-11-09 17:14:43 +0100

    feat: add main script and README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# ps1-customizer
-Temporary $PS1 customizer made for a bash scripting course
+# PS1 Customizer
+
+I wrote this script as an assignment for a Linux Shell Programming [course](https://www.uc3m.es/lifelong-learning/Microcredencial-Programacion-Shell-Linux). It is a scuffed version of this Bash Prompt Generator [website](https://bash-prompt-generator.org), but in the form of a bash script. The quality of the code may be underwhelming.
+
+## How to run
+
+Run the script through:
+```bash
+source ps1-customizer.sh
+# or
+. ps1-customizer.sh
+```
+This will execute the script without creating a subshell.
+
+If you already have a custom $PS1 in your `.bashrc`, you will have to do
+```bash
+bash --norc
+```
+before executing the script.
+
+## Notes
+
+Dim, italic and underline text styles have limited support.
+
+If you have a custom color scheme set up for your terminal, it is possible that the colors won't correspond to what they are labeled as.
+
+Currently, the script doesn't apply the change to your `.bashrc`, but it instead gives a command you can add in it to change your prompt permanently. However, it does change the `$PS1` variable of the shell it is executed in, so long as your `.bashrc` is not overwriting it. During execution, it will also show you a preview of the new prompt you are creating.
+
+In the preview, the elements of the prompt are shown as their escape sequences (`\u`, `\n`, `$?`, etc.)
+
+I will (maybe) update it and add more features in the future.
---
+++ ps1-customizer.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+NEW_PROMPT=""
+
+declare -A COLOR_CODES=(
+  ["white"]="\[\e[37m\]"
+  ["black"]="\[\e[30m\]"
+  ["red"]="\[\e[31m\]"
+  ["green"]="\[\e[32m\]"
+  ["blue"]="\[\e[34m\]"
+  ["cyan"]="\[\e[36m\]"
+  ["yellow"]="\[\e[33m\]"
+  ["magenta"]="\[\e[35m\]"
+  ["default"]="\[\e[0m\]"
+)
+
+declare -A STYLE_CODES=(
+  ["bold"]="\[\e[1m\]"
+  ["dim"]="\[\e[2m\]"
+  ["italic"]="\[\e[3m\]"
+  ["underline"]="\[\e[4m\]"
+  ["default"]="\[\e[0m\]"
+)
+
+main_menu() {
+  clear
+  
+  if [ -n "$NEW_PROMPT" ]; then
+    echo "Prompt preview:"
+    display_prompt="${NEW_PROMPT//\\[/}"
+    display_prompt="${display_prompt//\\]/}"
+    echo -e "${display_prompt}"
+    echo ""
+  fi
+  
+  cat << EOF
+Select an option:
+  1) Add new element
+  2) Reset
+  3) Save and quit
+  4) Quit without saving
+EOF
+  
+  read -n 1 -r -s -p "Press the corresponding key: " choice_menu
+  echo ""
+  
+  case $choice_menu in
+    1) insert_element ;;
+    2) NEW_PROMPT=""; echo "Prompt reset."; sleep 1 ;;
+    3) save_prompt; return 1 ;;
+    4) printf "Exiting...\n"; return 1 ;;
+    *) echo "Invalid option"; sleep 1 ;;
+  esac
+}
+
+insert_element() {
+  local element color style
+  
+  element=$(select_element)
+  [ -z "$element" ] && return
+  
+  color=$(choose_color)
+  [ -z "$color" ] && return
+  
+  style=$(choose_style)
+  [ -z "$style" ] && return
+  
+  NEW_PROMPT="${NEW_PROMPT}${COLOR_CODES[$color]}${STYLE_CODES[$style]}${element}\[\e[0m\]"
+}
+
+save_prompt() {
+  clear
+  
+  PS1="${NEW_PROMPT}"
+  echo "Prompt has been updated for this terminal session."
+  echo "To make it permanent, add this line to your ~/.bashrc:"
+  echo "PS1='${NEW_PROMPT}'"
+}
+
+select_element() {
+  clear >&2
+  
+  cat << EOF >&2
+Select an element to add:
+ 1) Username
+ 2) Hostname
+ 3) Current directory
+ 4) Bash release
+ 5) Exit status
+ 6) Time
+ 7) Date
+ 8) Newline
+ 9) Prompt sign
+ 0) Custom text
+ r) Go back
+EOF
+  
+  read -n 1 -r -s -p "Press the corresponding key: " choice_element >&2
+  echo "" >&2
+  
+  case $choice_element in
+    1) echo '\u' ;;
+    2) echo '\h' ;;
+    3) echo '\w' ;;
+    4) echo '\r' ;;
+    5) echo '$?' ;;
+    6) echo '\t' ;;
+    7) echo '\d' ;;
+    8) echo '\n' ;;
+    9) echo '\$' ;;
+    0) read -rp "Enter custom text: " custom && echo "$custom" ;;
+    r|R) return ;;
+    *) echo "Invalid option"; sleep 1; select_element ;;
+  esac
+}
+
+choose_color() {
+  clear >&2
+  
+  cat << EOF >&2
+Choose a color:
+  1) White      6) Cyan
+  2) Black      7) Yellow
+  3) Red        8) Magenta
+  4) Green      9) Default
+  5) Blue
+EOF
+  
+  read -n 1 -r -s -p "Press the corresponding key: " choice_color >&2
+  echo "" >&2
+  
+  case $choice_color in
+    1) echo "white" ;;
+    2) echo "black" ;;
+    3) echo "red" ;;
+    4) echo "green" ;;
+    5) echo "blue" ;;
+    6) echo "cyan" ;;
+    7) echo "yellow" ;;
+    8) echo "magenta" ;;
+    9) echo "default" ;;
+    *) echo "Invalid option"; sleep 1; choose_color ;;
+  esac
+}
+
+choose_style() {
+  clear >&2
+  
+  cat << EOF >&2
+Choose a style:
+  1) Bold
+  2) Dim
+  3) Italic
+  4) Underline
+  5) Default
+EOF
+  
+  read -n 1 -r -s -p "Press the corresponding key: " choice_style >&2
+  echo "" >&2
+  
+  case $choice_style in
+    1) echo "bold" ;;
+    2) echo "dim" ;;
+    3) echo "italic" ;;
+    4) echo "underline" ;;
+    5) echo "default" ;;
+    *) echo "Invalid option"; sleep 1; choose_style ;;
+  esac
+}
+
+while true; do
+  main_menu
+  
+  if [ $? -eq 1 ]; then
+    break
+  fi
+done
